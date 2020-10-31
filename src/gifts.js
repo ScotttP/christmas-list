0d58f84a89ac9209593234e514d4d7abb4aa8eb4
Scott Pribe <Scottpribe@gmail.com>
2020-10-31 09:39:51 -0400

    need to add more gifts and do some styling and then should be all set

--- src/gifts.js
+++ src/gifts.js
@@ -3,11 +3,17 @@ import React from "react";
 export default function Gifts(props) {
 	return (
 		<div>
+			<img
+				class="giftImages"
+				id={"imageFor" + props.gifts[props.index].name}
+				src={props.gifts[props.index].image}
+				alt={props.gifts[props.index].name}
+			></img>
 			<h4>{props.gifts[props.index].name}</h4>
 			<h4>{props.gifts[props.index].price}</h4>
-			<h4>{props.gifts[props.index].notes}</h4>
+			<p>{props.gifts[props.index].notes}</p>
 			<a href={props.gifts[props.index].link} target="_blank" rel="noreferrer">
-				Link to Product
+				Link to Gift
 			</a>
 		</div>
 	);
